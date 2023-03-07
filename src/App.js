5ec455fbd24181174f31ddf291751332fc96f394
Hicbik <1959453433@qq.com>
2023-03-07 19:15:22 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useEffect} from 'react';
+import {useSetState} from 'react-use'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [state, setState] = useSetState({
+    list: [],
+    loading: false,
+
+    previewUrl: '',
+    previewVisible: false
+  })
+
+  const getData = async () => {
+    setState({loading: true})
+
+    const res = await new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuPphLU_rw211Ix3Sd49p8VeJDvm63hrDLFg&usqp=CAU',
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuPphLU_rw211Ix3Sd49p8VeJDvm63hrDLFg&usqp=CAU',
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuPphLU_rw211Ix3Sd49p8VeJDvm63hrDLFg&usqp=CAU'
+        ])
+      }, 1000)
+    })
+
+    setState(prevState => ({
+      list: prevState.list.concat(res),
+      loading: false
+    }))
+  }
+
+
+  const loadMore = () => {
+    const isBottom = document.documentElement.scrollTop + window.innerHeight  ===  document.documentElement.offsetHeight
+    if(isBottom){
+      getData().then()
+    }
+  }
+
+  const onPreview = useCallback((imgUrl) => {
+    setState({
+      previewUrl: imgUrl,
+      previewVisible: true
+    })
+  }, [setState])
+
+  useEffect(() => {
+    getData().then()
+    window.addEventListener('scroll', loadMore)
+    return () => {
+      window.removeEventListener('scroll', loadMore)
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <div
+            className="preview" style={{display: state.previewVisible ? 'flex' : 'none'}}
+            onClick={() => setState({previewVisible: false})}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <img src={state.previewUrl} alt=""/>
+        </div>
+
+        <div style={{padding: 20}}>
+          {state.list.map((item, index) => (
+              <Item key={index} onClick={() => onPreview(item)}/>
+          ))}
+        </div>
+      </div>
   );
+};
+
+const Item = ({onClick}) => {
+  return (
+      <div className="box-wrapper" onClick={onClick}>
+        <div className="item">
+          1
+        </div>
+
+        <div className="box-child-wrapper">
+          <div className="item">1</div>
+          <div className="item">1</div>
+          <div className="item">1</div>
+        </div>
+      </div>
+  )
 }
 
 export default App;
